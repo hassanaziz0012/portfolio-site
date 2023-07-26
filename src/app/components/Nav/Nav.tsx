c438d346ea122d9b2e31329110d53903332fdff3
Hassan Aziz <hassanaziz0012@gmail.com>
2023-07-27 00:14:22 +0500

    Make nav responsive

--- src/app/components/Nav/Nav.tsx
+++ src/app/components/Nav/Nav.tsx
@@ -1,14 +1,41 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import NavItem from '../NavItem/NavItem'
 
 export default function Nav() {
+    const [width, setWidth] = useState<number>(1920);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const handleWidthChange = () => {
+        setWidth(window.innerWidth);
+        console.log(width);
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleWidthChange);
+        return () => {
+            window.removeEventListener('resize', handleWidthChange)
+        }
+    }, [handleWidthChange])
+
+    useEffect(() => setWidth(window.innerWidth), [])
+
     return (
-        <div className="nav-links col-md-2 offset-md-1">
-            <ul className="nav-list">
-                <NavItem text='Projects' url='#projects' />
-                <NavItem text='Skills' url='#skills' />
-                <NavItem text='Contact' url='#contact' />
-            </ul>
+        <div className="nav-links col-md-3 offset-md-1">
+            {
+            width > 768 ? (
+                <ul className="nav-list">
+                    <NavItem text='Projects' url='#projects' />
+                    <NavItem text='Skills' url='#skills' />
+                    <NavItem text='Contact' url='#contact' />
+                </ul>
+                )
+                :
+                <div className="nav-list row col-lg-6 mt-3 mb-5">
+                    <NavItem text='Projects' url='#projects' />
+                    <NavItem text='Skills' url='#skills' />
+                    <NavItem text='Contact' url='#contact' />
+                </div>
+            }
         </div>
     )
 }
