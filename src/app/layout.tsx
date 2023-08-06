295fdae8035fc483a860d83f7cde9a6dfe7d7ccb
Hassan Aziz <hassanaziz0012@gmail.com>
2023-08-06 17:27:57 +0500

    Move bootstrap and fontawesome scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import './css/nord.css';
+import './css/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Script from 'next/script';
 
 
 export const metadata: Metadata = {
@@ -15,7 +19,9 @@ export default function RootLayout({
 
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body className='dark'>{children}</body>
+            <Script src='https://kit.fontawesome.com/8f8a8c35a9.js' />
+            <Script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js' />
         </html>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
-import './css/nord.css';
-import './css/style.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import ThemeChangeButton from './components/ThemeChangeButton/ThemeChangeButton';
-import Script from 'next/script';
 import About from './components/About/About';
-import Projects from './components/Projects/Projects';
-import { createDefaultProjects } from '../../prisma/db/project';
+import Projects from './components/ProjectsGrid/ProjectsGrid';
 import Contact from './components/Contact/Contact';
 import Skills from './components/Skills/Skills';
 
 export default function Home() {
-    // createDefaultProjects();
     return (
         <main>
             <ThemeChangeButton />
@@ -25,9 +19,6 @@ export default function Home() {
                     <p>&copy; Hassan Aziz - Thanks for stopping by!</p>
                 </div>
             </section>
-
-            <Script src='https://kit.fontawesome.com/8f8a8c35a9.js' />
-            <Script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js' />
 
         </main>
     )
