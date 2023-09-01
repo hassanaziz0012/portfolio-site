301743f48abf59639815cc4a0766ca8f0673992f
Hassan Aziz <hassanaziz0012@gmail.com>
2023-09-01 14:39:09 +0500

    Linking to project detail page

--- src/app/components/Project/Project.tsx
+++ src/app/components/Project/Project.tsx
@@ -28,7 +28,16 @@ export default function Project({ project, index }: {project: any, index: number
             }
 
             <div className={`col-lg-6 project-info ${width < 992 ? "mt-3" : ""}`}>
-                <h4>{project.title}</h4>
+                <h4>
+                    {project.detail_url ? (
+                        <a href={project.detail_url} className="link yellow-link">
+                            {project.title}
+                        </a>
+                        )
+                        :
+                        <>{project.title}</>
+                    }
+                </h4>
                 <p className="text-start" style={{ whiteSpace: 'pre-line' }} dangerouslySetInnerHTML={{ __html: project.description }} />
                 <hr />
                 <div className="col-md-12 row">
