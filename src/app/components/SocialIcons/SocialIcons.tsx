26c7aad9277147ceee4d16cff2d9fb23364d35be
Hassan Aziz <hassanaziz0012@gmail.com>
2023-10-01 17:39:41 +0500

    Clean up code

--- src/app/components/SocialIcons/SocialIcons.tsx
+++ src/app/components/SocialIcons/SocialIcons.tsx
@@ -1,25 +1,18 @@
+import socialLinks from '@/utils/socialLinks'
 import React from 'react'
+import GlassIcons from '../common/GlassIcons/GlassIcons'
 
-export default function SocialIcons() {
-    const github_url = "https://github.com/hassanaziz0012"
-    const email_url = "mailto:hassanaziz0012@gmail.com"
-    const discord_url = "https://discord.gg/vf3Sa83A7t"
-    const twitter_url = "https://twitter.com/itshassanaziz"
-
+export default function SocialIcons({ style }) {
     return (
+        style === "glass" ?
+        <GlassIcons icons={socialLinks} />
+        :
         <div className="social-links">
-            <a href={github_url} target='_blank' className="social-link github">
-                <i className="fa-brands fa-github"></i>
-            </a>
-            <a href={email_url} className="social-link email">
-                <i className="fa-solid fa-envelope"></i>
-            </a>
-            <a href={discord_url} target='_blank' className="social-link discord">
-                <i className="fa-brands fa-discord"></i>
-            </a>
-            <a href={twitter_url} className="social-link twitter">
-                <i className="fa-brands fa-twitter"></i>
-            </a>
+            {socialLinks.map((link, i) => (
+                <a href={link.url} target='_blank' key={i} className='social-link'>
+                    <i className={link.icon}></i>
+                </a>
+            ))}
         </div>
     )
 }
