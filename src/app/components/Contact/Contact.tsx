daa1a6b9907890412bd4493809729b1bfa3c0d15
Hassan Aziz <hassanaziz0012@gmail.com>
2023-10-01 17:37:24 +0500

    Content changes

--- src/app/components/Contact/Contact.tsx
+++ src/app/components/Contact/Contact.tsx
@@ -1,27 +1,41 @@
 import React from 'react'
 import SocialIcons from '../SocialIcons/SocialIcons'
+import Card from '../common/Card/Card'
+import socialLinks from '@/utils/socialLinks'
+import './Contact.css'
 
 export default function Contact() {
+    const emailUrl = socialLinks.filter((link) => link.name === "Email")[0].url
+    const discordUrl = socialLinks.filter((link) => link.name === "Discord")[0].url
+
     return (
-        <section id="contact" className="contact">
-            <div className="contact-section container d-flex flex-column align-items-center justify-content-center pt-3">
-                <h1 className="section-heading">Contact</h1>
-                <div>
-                    <p className="text-center">
-                        Wanna talk to me about a project?
-                    </p>
+        <section className="contact d-flex flex-column align-items-center justify-content-center">
+            <div className="contact-section pb-5 pt-5">
+                <h1 className="section-heading text-center">Contact</h1>
+                <div className="row container pt-3 d-flex">
+                    <div className="offset-lg-4 col-lg-4">
+                        <Card>
+                            <h4>Get in touch</h4>
 
-                    <div className="text-center">
-                        <SocialIcons />
+                            <p className='mb-0'>Send an email</p>
+                            <a href={emailUrl} className='link'>{emailUrl.replace("mailto:", "")}</a>
+
+                            <p className='mb-0 mt-3'>Chat on Discord</p>
+                            <a href={discordUrl} className='link'>Join my server</a>
+
+                            <p className='mb-0 mt-3'>My social medias</p>
+                            <SocialIcons style="default" />
+                        </Card>
                     </div>
-
-                    <p className="text-center">
-                        The best way to get in touch is Discord: <a className="link yellow-link"
-                            href="https://discord.gg/vf3Sa83A7t">Hassan#3557</a>
-                        - I&apos;m most active there.
-                        If that&apos;s not your thing, you can email me at <a className="link yellow-link" 
-                            href="mailto:hassanaziz0012@gmail.com">hassanaziz0012@gmail.com</a>
-                    </p>
+                    <div className="col-lg-4">
+                        <p>
+                            The best way to get in touch is Discord: <a className="link primary-link"
+                                href="https://discord.gg/vf3Sa83A7t">Hassan#3557 (itshassanaziz)</a>
+                            - I&apos;m most active there.
+                            If that&apos;s not your thing, you can email me at <a className="link primary-link"
+                                href="mailto:hassanaziz0012@gmail.com">hassanaziz0012@gmail.com</a>
+                        </p>
+                    </div>
                 </div>
             </div>
         </section>
