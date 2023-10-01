1eda5267ae81bcf309b4e04204888948c8495073
Hassan Aziz <hassanaziz0012@gmail.com>
2023-10-01 17:34:01 +0500

    Add fade in animation + minor content changes

--- src/app/components/About/About.tsx
+++ src/app/components/About/About.tsx
@@ -1,28 +1,38 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import Nav from '../HomeNav/HomeNav'
 import SocialIcons from '../SocialIcons/SocialIcons'
+import HLoader from '../common/HLoader/HLoader'
+import './About.css';
+import NeonButton from '../common/NeonButton/NeonButton'
+
+
+const aboutInfo = {
+    headline: "I'm Hassan",
+    subheadline: "I build software.",
+    description: `I'm a self-taught developer and have been writing code for 4 years now. During that time, I've built
+                    everything from websites, automation scripts, and discord bots to APIs, desktop and mobile apps,
+                    and more. I really enjoy programming and building cool stuff from just a text file.`
+}
 
 export default function About() {
     return (
-        <section className="hero d-flex align-items-center justify-content-center">
+        <section className="hero about-section d-flex align-items-center justify-content-center">
             <div className="row d-flex align-items-center justify-content-center container">
                 <div className="hero-section col-md-6">
-                    <h1>I&apos;m Hassan</h1>
-                    <h4>I build software.</h4>
-                    <p>
-                        I&apos;m a self-taught developer and have been writing code for 4 years now. During that time, I&apos;ve built
-                        everything from websites, automation scripts, and discord bots to APIs, desktop and mobile apps,
-                        and more. I really enjoy programming and building cool stuff from just a text file.
-                    </p>
+                    <h1>{aboutInfo.headline}</h1>
+                    <h4>{aboutInfo.subheadline}</h4>
+                    <p>{aboutInfo.description}</p>
 
                     <div className="hero-buttons mt-4">
-                        <button className="btn btn-secondary" style={{ borderColor: "#3B4252" }}>
+                        {/* <button className="btn btn-secondary" style={{ borderColor: "#3B4252" }}>
                             <a className='link' href="resume.pdf" style={{ background: "transparent", color: "var(--light-background-1)" }}>Download Resume</a>
-                        </button>
+                        </button> */}
+                        <NeonButton text="Download Resume" url="resume.pdf" />
                     </div>
 
                     <div className="mt-3">
-                        <SocialIcons />
+                        <SocialIcons style={"default"} />
                     </div>
                 </div>
 
