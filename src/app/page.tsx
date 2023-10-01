2692c63811785dbecf44b4aa63d9211776e16704
Hassan Aziz <hassanaziz0012@gmail.com>
2023-10-01 17:34:46 +0500

    About on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,22 @@ import About from './components/About/About';
 import Projects from './components/ProjectsGrid/ProjectsGrid';
 import Contact from './components/Contact/Contact';
 import Skills from './components/Skills/Skills';
+import AboutPage from './pages/AboutPage';
+
 
 export default function Home() {
     return (
         <main>
-
-            <About />
-            <Projects />
+            <AboutPage />
+            {/* <Projects />
             <Skills />
-            <Contact />
+            <Contact /> */}
 
-            <section className="footer">
+            {/* <section className="footer">
                 <div className="footer-content d-flex flex-column align-items-center justify-content-center pt-3">
                     <p>&copy; Hassan Aziz - Thanks for stopping by!</p>
                 </div>
-            </section>
+            </section> */}
 
         </main>
     )
