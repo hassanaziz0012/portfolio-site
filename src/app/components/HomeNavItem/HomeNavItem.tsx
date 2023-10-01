52a4e0cf3fd0aeee04c54a409b18e0751c6b3e20
Hassan Aziz <hassanaziz0012@gmail.com>
2023-10-01 17:39:06 +0500

    Formatting

--- src/app/components/HomeNavItem/HomeNavItem.tsx
+++ src/app/components/HomeNavItem/HomeNavItem.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function NavItem({ text, url }: {text: string, url: string}) {
     return (
-        <li className="nav-item"><a href={url} className="nav-link">{text}</a></li>
+        <li className="nav-item">
+            <a href={url} className="nav-link">{text}</a>
+        </li>
     )
 }
