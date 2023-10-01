6ebc79eac73f8b14c4a0ed3f97046a4da8063eae
Hassan Aziz <hassanaziz0012@gmail.com>
2023-10-01 17:32:14 +0500

    Organize social links

---
+++ src/utils/socialLinks.ts
@@ -0,0 +1,28 @@
+const socialLinks = [
+    {
+        name: "Github",
+        icon: "fa-brands fa-github",
+        url: "https://github.com/hassanaziz0012",
+        rotateDeg: -15
+    },
+    {
+        name: "Email",
+        icon: "fa-solid fa-envelope",
+        url: "mailto:hassanaziz0012@gmail.com",
+        rotateDeg: 5
+    },
+    {
+        name: "Discord",
+        icon: "fa-brands fa-discord",
+        url: "https://discord.gg/vf3Sa83A7t",
+        rotateDeg: 25
+    },
+    {
+        name: "Twitter",
+        icon: "fa-brands fa-twitter",
+        url: "https://twitter.com/itshassanaziz",
+        rotateDeg: -15
+    }
+]
+
+export default socialLinks;
