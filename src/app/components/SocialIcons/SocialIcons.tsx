9f8e4c756201e0a1358052e1493c4486ae1f6427
Hassan Aziz <hassanaziz0012@gmail.com>
2023-10-02 21:46:06 +0500

    Configure nohover style

--- src/app/components/SocialIcons/SocialIcons.tsx
+++ src/app/components/SocialIcons/SocialIcons.tsx
@@ -1,13 +1,14 @@
 import socialLinks from '@/utils/socialLinks'
 import React from 'react'
 import GlassIcons from '../common/GlassIcons/GlassIcons'
+import './SocialIcons.css'
 
 export default function SocialIcons({ style }) {
     return (
         style === "glass" ?
         <GlassIcons icons={socialLinks} />
         :
-        <div className="social-links">
+        <div className={`social-links ${style}`}>
             {socialLinks.map((link, i) => (
                 <a href={link.url} target='_blank' key={i} className='social-link'>
                     <i className={link.icon}></i>
