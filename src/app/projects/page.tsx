2d66004566073d7fbe4fb0afe1581e075e6f00b3
Hassan Aziz <hassanaziz0012@gmail.com>
2023-10-02 21:45:49 +0500

    Show all projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 }
 
 export default async function page() {
-    const projects = await getFeaturedProjects();
+    const projects = await getAllProjects();
     
     return (
         <Page>
