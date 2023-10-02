6aa8ee97770884c4d4b9bb50ca160d03b87e4ebb
Hassan Aziz <hassanaziz0012@gmail.com>
2023-10-02 14:40:23 +0500

    Organize skill icons

--- src/app/components/Skills/Skills.tsx
+++ src/app/components/Skills/Skills.tsx
@@ -14,72 +14,72 @@ export default function Skills() {
 
                 <ul className="skills-list d-flex flex-wrap col-lg-9">
                     <li className="skill-tag">
-                        <img src="react.png" alt="React icon" className="skill-icon img-fluid" />
+                        <img src="/skills/react.png" alt="React icon" className="skill-icon img-fluid" />
                             ReactJS
                     </li>
 
                     <li className="skill-tag">
-                        <img src="node-js.png" alt="NodeJS icon" className="skill-icon img-fluid" />
+                        <img src="/skills/node-js.png" alt="NodeJS icon" className="skill-icon img-fluid" />
                             NodeJS
                     </li>
 
                     <li className="skill-tag">
-                        <img src="next-js.svg" alt="NextJS icon" className="skill-icon img-fluid" />
+                        <img src="/skills/next-js.svg" alt="NextJS icon" className="skill-icon img-fluid" />
                             NextJS
                     </li>
 
                     <li className="skill-tag">
-                        <img src="js.png" alt="Javascript icon" className="skill-icon img-fluid" />
+                        <img src="/skills/js.png" alt="Javascript icon" className="skill-icon img-fluid" />
                             Javascript
                     </li>
 
                     <li className="skill-tag">
-                        <img src="python.png" alt="Python icon" className="skill-icon img-fluid" />
+                        <img src="/skills/python.png" alt="Python icon" className="skill-icon img-fluid" />
                             Python
                     </li>
 
                     <li className="skill-tag">
-                        <img src="django.png" alt="Django icon" className="skill-icon img-fluid" />
+                        <img src="/skills/django.png" alt="Django icon" className="skill-icon img-fluid" />
                             Django
                     </li>
 
                     <li className="skill-tag">
-                        <img src="postgre.png" alt="Postgres icon" className="skill-icon img-fluid" />
+                        <img src="/skills/postgre.png" alt="Postgres icon" className="skill-icon img-fluid" />
                             Postgres
                     </li>
 
                     <li className="skill-tag">
-                        <img src="mongo.png" alt="MongoDB icon" className="skill-icon img-fluid" />
+                        <img src="/skills/mongo.png" alt="MongoDB icon" className="skill-icon img-fluid" />
                             MongoDB
                     </li>
 
                     <li className="skill-tag">
-                        <img src="docker.png" alt="Docker icon" className="skill-icon img-fluid" />
+                        <img src="/skills/docker.png" alt="Docker icon" className="skill-icon img-fluid" />
                             Docker
                     </li>
 
                     <li className="skill-tag">
-                        <img src="aws.png" alt="AWS icon" className="skill-icon img-fluid" />
+                        <img src="/skills/aws.png" alt="AWS icon" className="skill-icon img-fluid" />
                             AWS
                     </li>
 
                     <li className="skill-tag">
-                        <img src="discord.png" alt="Discord icon" className="skill-icon img-fluid" />
+                        <img src="/skills/discord.png" alt="Discord icon" className="skill-icon img-fluid" />
                             Discord Bots
                     </li>
 
                     <li className="skill-tag">
-                        <img src="gnu-bash.png" alt="Bash icon" className="skill-icon img-fluid" />
+                        <img src="/skills/gnu-bash.png" alt="Bash icon" className="skill-icon img-fluid" />
                             Bash
                     </li>
 
                     <li className="skill-tag">
-                        <img src="apache.png" alt="Apache icon" className="skill-icon img-fluid" />
+                        <img src="/skills/apache.png" alt="Apache icon" className="skill-icon img-fluid" />
                             Apache
                     </li>
 
                     <li className="skill-tag">
-                        <img src="nginx.png" alt="Nginx icon" className="skill-icon img-fluid" />
+                        <img src="/skills/nginx.png" alt="Nginx icon" className="skill-icon img-fluid" />
                             Nginx
                     </li>
 
